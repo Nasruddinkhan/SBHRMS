6d8f6da8ab492288a3c3e108d2990d75c133760e
Nasruddinkhan <nasruddinkhan44@gmail.com>
2019-08-04 17:31:01 +0530

    remove source code

--- HRMS/src/main/java/com/mypractice/hrms/HRMSApplication.java
+++ HRMS/src/main/java/com/mypractice/hrms/HRMSApplication.java
@@ -1,12 +1,10 @@
 package com.mypractice.hrms;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.mypractice.hrms.repository.SkillElementRepo;
 import com.mypractice.hrms.util.MailApiUtils;
 
 /**
@@ -16,14 +14,12 @@ import com.mypractice.hrms.util.MailApiUtils;
 @SpringBootApplication
 public class HRMSApplication {
 	
-	@Autowired
-	SkillElementRepo skillrepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HRMSApplication.class, args);
 	}
 		@Bean
 		CommandLineRunner lookup(MailApiUtils mail) {
-			skillrepo.getSkillElementsDetails().forEach(obj->System.out.println(obj.toString()));
 			return args -> {
 				//System.err.println(mail.sendEmail(MailProperty.TEST_EMAILTEMPLATE, mailModel));
 			};
