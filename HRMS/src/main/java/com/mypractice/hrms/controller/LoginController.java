ebe15b63ab497606b40bec8046c166b2fdeca871
Nasruddinkhan <nasruddinkhan44@gmail.com>
2019-09-28 03:00:54 +0530

    check user authorization

--- HRMS/src/main/java/com/mypractice/hrms/controller/LoginController.java
+++ HRMS/src/main/java/com/mypractice/hrms/controller/LoginController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mypractice.hrms.bean.DropDownBean;
 import com.mypractice.hrms.bean.LoginBO;
 import com.mypractice.hrms.bean.ResponseMessage;
 import com.mypractice.hrms.model.SubMenus;
@@ -60,6 +61,7 @@ import io.swagger.annotations.ApiParam;
 		 ResponseMessage msg=new ResponseMessage();
 		 // TODO Auto-generated method stub
 		 User details = loginService.checkDBLogin(loginBO);
+		 List<DropDownBean>  accessMenus = null;
 		 Predicate<User> check= u -> u != null;
 		 if(check.test(details)) {
 			 Predicate<String> checkStatus= s -> approved.intern() ==s.intern() ;
@@ -69,9 +71,16 @@ import io.swagger.annotations.ApiParam;
 				 msg.setErrorCode(errorApply .apply(details.getUserType()));
 				 msg.setErrorMessage("Success");
 				 msg.setToken(jwtGenerator.generate(loginBO));
-				 Map<String, List<SubMenus>> acceessMenu = new HashMap<String, List<SubMenus>>();
+				 Map<String, List<DropDownBean>> acceessMenu = new HashMap<String, List<DropDownBean>>();
 				 List<String>  suBmenus =menuRepo.findAllSubMenus(details).stream().map(c->c.getMenu().getSubMenuId()).collect(Collectors.toList());
-				 acceessMenu.put("menus", subMenuRepo.findAll(suBmenus));
+				 if(suBmenus !=null && suBmenus.size()>0) 
+					 accessMenus=  subMenuRepo.findAll(suBmenus).stream().map(o->{
+						   DropDownBean drop = new DropDownBean();
+						   drop.setId(o.getSubMenuId());
+						   drop.setCode( o.getSubMenuName());
+						   return drop;
+					 }).collect(Collectors.toList());
+				 acceessMenu.put("menus", accessMenus);
 				 msg.setMap(acceessMenu);
 				 msg.setUser(details);
 				 return new ResponseEntity<>(msg,HttpStatus.OK);
